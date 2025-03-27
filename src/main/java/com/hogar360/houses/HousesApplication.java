0110792f08a7ff3d7617031caec5a3a75ce5263c
Laura <laura.barona@uao.edu.co>
2025-03-27 16:47:47 -0500

    Initialize Spring Boot project for Houses microservice

---
+++ src/main/java/com/hogar360/houses/HousesApplication.java
@@ -0,0 +1,13 @@
+package com.hogar360.houses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousesApplication.class, args);
+	}
+
+}
